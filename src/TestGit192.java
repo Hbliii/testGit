3953c0bc0d2910d9647d4a0c500610f76cd632b3
Hbliii <724637652@qq.com>
2021-12-19 16:24:17 +0800

    李四的修改

--- src/TestGit192.java
+++ src/TestGit192.java
@@ -1,6 +1,6 @@
 public class TestGit192 {
     public static void main(String[] args){
-
+    stuinfo("002","李四");
     }
     public  static void stuinfo(String sid, String sname){
         System.out.println("学号："+ sid + "姓名："+ sname);
